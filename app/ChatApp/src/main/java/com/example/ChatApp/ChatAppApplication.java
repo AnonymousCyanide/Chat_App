03108afff8d60960ce40fabff6c145bb948c23ce
Rahul Khajanchi <rahulkhajanchi121@gmai.com>
2025-08-03 18:04:24 +0530

    added java files

---
+++ app/ChatApp/src/main/java/com/example/ChatApp/ChatAppApplication.java
@@ -0,0 +1,18 @@
+package com.example.ChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@Slf4j
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+		
+		log.info("Backend Server started on port 8080");
+	}
+}
